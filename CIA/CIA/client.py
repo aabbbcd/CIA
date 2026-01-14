4f11346334c689395f554a9815559e88582458f7
aabbbcd <72444266+aabbbcd@users.noreply.github.com>
2026-01-14 13:34:33 +0800

    Update client.py

--- CIA/CIA/client.py
+++ CIA/CIA/client.py
@@ -8,63 +8,7 @@ _model = None
 _model_device = None
 
 
-# def load_llm(model_path: str, device: str = "cuda:3", torch_dtype: str = "auto"):
-# 	global _tokenizer, _model, _model_device
-# 	if _model is not None:
-# 		return _tokenizer, _model
 
-# 	# 解析设备
-# 	if device.startswith("cuda") and torch.cuda.is_available():
-# 		# 支持 cuda 或 cuda:N
-# 		_device = device
-# 	else:
-# 		_device = "cpu"
-
-# 	# dtype
-# 	if torch_dtype == "auto":
-# 		dtype = torch.float16 if _device.startswith("cuda") else torch.float32
-# 	elif torch_dtype == "float16":
-# 		dtype = torch.float16
-# 	elif torch_dtype == "bfloat16":
-# 		dtype = torch.bfloat16
-# 	else:
-# 		dtype = torch.float32
-
-# 	_tokenizer = AutoTokenizer.from_pretrained(model_path)
-# 	_model = AutoModelForCausalLM.from_pretrained(
-# 		model_path,
-# 		torch_dtype=dtype,
-# 		low_cpu_mem_usage=True,
-# 		device_map="auto" if _device.startswith("cuda") else None
-# 	)
-# 	if not _device.startswith("cuda"):
-# 		_model = _model.to(_device)
-# 	_model.eval()
-# 	_model_device = _device
-# 	return _tokenizer, _model
-
-
-# def generate(prompt: str,
-# 			 model_path: str,
-# 			 device: str = "cuda:3",
-# 			 max_new_tokens: int = 512,
-# 			 temperature: float = 0.01,
-# 			 do_sample: bool = False) -> str:
-# 	"""生成文本（不加载多次模型，走单例缓存）。"""
-# 	tok, mdl = load_llm(model_path=model_path, device=device)
-# 	inputs = tok(prompt, return_tensors="pt")
-# 	inputs = {k: v.to(mdl.device) for k, v in inputs.items()}
-# 	with torch.no_grad():
-# 		out = mdl.generate(
-# 			**inputs,
-# 			max_new_tokens=max_new_tokens,
-# 			temperature=max(1e-3, float(temperature)),
-# 			do_sample=do_sample,
-# 			pad_token_id=tok.eos_token_id,
-# 			eos_token_id=tok.eos_token_id
-# 		)
-# 	text = tok.decode(out[0], skip_special_tokens=True)
-# 	return text
 
 def generate(messages: str,
 			 model_path: str,
@@ -111,7 +55,7 @@ def generate(messages: str,
 				return text
 
 			except Exception as e:
-				return f"生成失败: {e}"
+				return f"error: {e}"
 		prompt = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
 		result = generate_text_transformers(prompt)
 		return result
